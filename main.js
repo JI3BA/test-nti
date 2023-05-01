b58c7c07b063f672368ffdc18b91b90cd0446848
JI3BA <lyovkin_artyom@mail.ru>
2023-05-02 01:12:51 +0300

    created validation form with some styles

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+////// DropDown
 const toogleDropDown = document.querySelector('.drop-down__change')
 const dropDown = document.querySelector('.drop-down')
 const windowAcount = document.querySelector('.personal')
@@ -9,3 +10,39 @@ toogleDropDown.addEventListener('mouseover', () => {
 windowAcount.addEventListener('mouseleave', () => {
     dropDown.classList.remove('drop-down--active')
 })
+
+////// Validation Contacts Form
+function validationForm(form){
+    let result = true
+    let amountValue = 0
+
+    form.querySelectorAll('.input').forEach(input => {
+        if(input.value.trim() === '' || input.value.trim().length < 6){
+            result = false
+            input.classList.add('input-box__input--invalid')
+            input.nextElementSibling.classList.add('input-box__name--invalid')
+        }else{
+            input.classList.remove('input-box__input--invalid')
+            input.nextElementSibling.classList.remove('input-box__name--invalid')
+            result = true
+            amountValue += 1
+        }
+
+    })
+
+    if(amountValue === 2){
+        form.querySelectorAll('.input').forEach(input => {
+            input.value = ''
+        })
+        return result
+    }
+}
+
+document.querySelector('.request__button').addEventListener('click', function(e){
+    e.preventDefault()
+
+    if(validationForm(document) === true){
+        alert('Данные отправлены')
+    }
+})
+
