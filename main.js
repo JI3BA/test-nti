6c72db68d07c043c73eec3e57321aebe759b852e
JI3BA <lyovkin_artyom@mail.ru>
2023-05-01 18:55:04 +0300

    created dropDown window and worked over it

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-import Swiper from 'swiper';
-import './styles/gallery.css'
+const toogleDropDown = document.querySelector('.drop-down__change')
+const dropDown = document.querySelector('.drop-down')
+const windowAcount = document.querySelector('.personal')
+
+toogleDropDown.addEventListener('mouseover', () => {
+    dropDown.classList.add('drop-down--active')
+})
+
+windowAcount.addEventListener('mouseleave', () => {
+    dropDown.classList.remove('drop-down--active')
+})
